256ba2b471b583cadbc3c0517575fe5427bee39b
lorindol <martin.brotzeller@mayflower.de>
2016-07-07 00:52:55 +0200

    Changed graph

--- app/src/main/java/net/brotzeller/peakflower/Charting.java
+++ app/src/main/java/net/brotzeller/peakflower/Charting.java
@@ -1,11 +1,21 @@
 package net.brotzeller.peakflower;
 
 
+import android.graphics.Color;
+
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.AxisValueFormatter;
+import com.github.mikephil.charting.formatter.FormattedStringCache;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 
 import java.text.DateFormat;
@@ -28,25 +38,124 @@ public class Charting {
     }
 
     public void chart() {
-        ArrayList<MeterStorage.DataPoint> data = storage.getData();
-        //ArrayList<String> xvals = new ArrayList<>();
-        ArrayList<Entry> yvals = new ArrayList<>();
-        long ts_now = System.currentTimeMillis() / 1000L / 60L;
-        long ts_then = ts_now - 60 * 24 * 14; // 14 days past
-        //DateFormat df = new SimpleDateFormat("m.d. HH:mm");
-        for(MeterStorage.DataPoint point : data) {
-            long xVal = point.getDate().getTime() / 1000L /60L ;
-            if (ts_then > xVal) {
-                continue;
-            }
-            yvals.add(new Entry(xVal, (float)point.getFlow()));
-        }
-        LineDataSet line = new LineDataSet(yvals, "Flow");
-        ArrayList<ILineDataSet> lines = new ArrayList<>();
-        lines.add(line);
-        LineData chartData = new LineData(lines);
+        chart.setDescription("");
+        chart.setNoDataTextDescription("You need to provide data for the chart.");
 
-        chart.setData(chartData);
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        chart.setDragDecelerationFrictionCoef(0.9f);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawGridBackground(false);
+        chart.setHighlightPerDragEnabled(true);
+
+        // set an alternative background color
+        chart.setBackgroundColor(Color.WHITE);
+        chart.setViewPortOffsets(0f, 0f, 0f, 0f);
+
+
+
+        setData();
         chart.invalidate();
-     }
+
+        Legend l = chart.getLegend();
+        l.setEnabled(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
+        //xAxis.setTypeface(mTfLight);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(true);
+        xAxis.setTextColor(Color.rgb(255, 192, 56));
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setGranularity(3600000L); // one Hour
+        xAxis.setValueFormatter(new AxisValueFormatter() {
+
+            private FormattedStringCache.Generic<Long, Date> mFormattedStringCache = new FormattedStringCache.Generic<>(new SimpleDateFormat("d.m. HH:mm"));
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                Long v = (long) value;
+                return mFormattedStringCache.getFormattedValue(new Date(v), v);
+            }
+
+            @Override
+            public int getDecimalDigits() {
+                return 0;
+            }
+        });
+
+        YAxis leftAxis = chart.getAxisLeft();
+        drawLimits(storage.getMax(), leftAxis);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+        //leftAxis.setTypeface(mTfLight);
+        leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGranularityEnabled(true);
+        leftAxis.setAxisMinValue((float)storage.getMax()*0.25f);
+        leftAxis.setAxisMaxValue(1.1f*(float)storage.getMax());
+        leftAxis.setYOffset(-9f);
+        leftAxis.setTextColor(Color.rgb(255, 192, 56));
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+    }
+    private void setData() {
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (MeterStorage.DataPoint point : storage.getData()) {
+            values.add(new Entry(point.getDate().getTime(), point.getFlow()));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(values, "Flow");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setValueTextColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(1.5f);
+        set1.setDrawCircles(false);
+        set1.setDrawValues(false);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        set1.setDrawCircleHole(false);
+
+        // create a data object with the datasets
+        LineData data = new LineData(set1);
+        data.setValueTextColor(Color.GRAY);
+        data.setValueTextSize(9f);
+
+        // set data
+        chart.setData(data);
+    }
+
+    private void drawLimits(int max, YAxis leftAxis) {
+        float upper = (float) max * 0.8f;
+        float lower = (float) max * 0.5f;
+        LimitLine ll1 = new LimitLine(upper, "Dosierung Einhalten");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        ll1.setTextSize(10f);
+        //ll1.setTypeface(tf);
+
+        LimitLine ll2 = new LimitLine(lower, "Ab zum Arzt!");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        ll2.setTextSize(10f);
+        //ll2.setTypeface(tf);
+        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+        leftAxis.addLimitLine(ll1);
+        leftAxis.addLimitLine(ll2);
+
+    }
+
+
 }
